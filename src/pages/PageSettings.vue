75c9c946454e063c9e395002ab9eb123c36bfd73
= <=>
2019-06-28 15:33:01 +0900

    pug typescript settings

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  q-page(padding)
+    p settings
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  q-page(padding)
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
